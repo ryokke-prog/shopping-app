69ecb72c02d54458d43a02df14af41a6ca704d94
ryota-develop <1930ajdu@gmail.com>
2026-04-20 12:07:48 +0900

    Initial commit: お買いもの PWA

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'shopping-v12';
+const FILES = ['/shopping-app/', '/shopping-app/index.html', '/shopping-app/manifest.json', '/shopping-app/512-icon.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
